b16b307a58ab2238e5acf93d8cca2e813c7ba135
Dmitry Tsapin <aw3472@gmail.com>
2025-03-23 16:57:17 +0300

    Дозалив

--- config/config.py
+++ config/config.py
@@ -1,11 +1,5 @@
 # Таски для обучения моделей
 
-
-# Названия SQL-таблиц для тасков
-task_tables_dict = {
-    "car_sales_pred": "car_sales",
-}
-
 # Колонки таргетов для тасков
 targets_dict = {
     "car_sales_pred": ["price"],
