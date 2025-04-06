93c2ec01f9e571cf8f0dfb7b10006d39b7cf2450
Dmitry Tsapin <aw3472@gmail.com>
2025-04-06 17:24:13 +0300

    Поправил config

--- config/config.py
+++ config/config.py
@@ -11,6 +11,7 @@ targets_dict = {
     "car_sales_pred": "price",
 }
 
+# Словарь для связи эндпоинтов с тасками
 models_mapping_dict = {
     "get_sale_prediction_for_one_car": "car_sales_pred",
     "get_sale_prediction_for_multiple_cars": "car_sales_pred",
